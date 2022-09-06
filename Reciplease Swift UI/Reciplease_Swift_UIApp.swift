b741db7058354bb66658b41d674b52db1d1aff39
Flo2503 <bruel.florent@gmail.com>
2022-09-06 18:57:23 +0200

    Initial Commit

---
+++ Reciplease Swift UI/Reciplease_Swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reciplease_Swift_UIApp.swift
+//  Reciplease Swift UI
+//
+//  Created by Florent on 06/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Reciplease_Swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
